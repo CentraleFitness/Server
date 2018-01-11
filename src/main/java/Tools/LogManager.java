40af26b8d8d27b2d5a53a1acf53ab082636c3210
Hadrien Daures <hadrien.daures@gmail.com>
2018-01-11 16:21:43 +0100

    LogManager

---
+++ src/main/java/Tools/LogManager.java
@@ -0,0 +1,20 @@
+package Tools;
+
+import java.io.Console;
+import java.io.PrintWriter;
+import java.util.Date;
+
+public class LogManager {
+
+    private PrintWriter mLog;
+
+    private static  LogManager INSTANCE = new LogManager();
+    public static LogManager getINSTANCE() {return INSTANCE;}
+    private LogManager() {
+        try {
+            mLog = new PrintWriter(new Date().toString(), "UTF-8");
+        } catch (Exception e) {
+            System.err.println("LogManager: Could not open file");
+        }
+    }
+}
