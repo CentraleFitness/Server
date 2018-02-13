f690c6c0c7be198522554c8d164f16870446a551
Hadrien Daures <hadrien.daures@gmail.com>
2018-02-13 16:10:56 +0100

    mobile pair stop

--- src/main/java/server/api/routes/mobile/UserPairStop.java
+++ src/main/java/server/api/routes/mobile/UserPairStop.java
@@ -21,7 +21,7 @@ import java.util.Map;
 
 public class UserPairStop {
     public UserPairStop(Router router) {
-        router.route(HttpMethod.POST, Protocol.Path.USER_GET_INSTANTPRODUCTION.path).handler(routingContext -> {
+        router.route(HttpMethod.POST, Protocol.Path.USER_PAIR_STOP.path).handler(routingContext -> {
 
             ResponseObject sending = null;
             HttpServerResponse response = routingContext.response().putHeader("content-type", "text/plain");
@@ -29,17 +29,10 @@ public class UserPairStop {
             label:try {
                 Map<String, Object> received = routingContext.getBodyAsJson().getMap();
                 String rToken = (String) received.get(Protocol.Field.TOKEN.key);
-                ObjectId rSessionId = (ObjectId) received.get(Protocol.Field.SESSIONID);
                 if (rToken == null) {
                     sending = new ResponseObject(true);
                     sending.put(Protocol.Field.STATUS.key, Protocol.Status.GENERIC_KO.code);
                     LogManager.write("Missing key " + Protocol.Field.TOKEN.key);
-                    break label;
-                }
-                if (rSessionId == null) {
-                    sending = new ResponseObject(true);
-                    sending.put(Protocol.Field.STATUS.key, Protocol.Status.GENERIC_KO.code);
-                    LogManager.write("Missing key " + Protocol.Field.SESSIONID.key);
                     break label;
                 }
                 User user = (User) Database.find_entity(Database.Collections.Users, User.Field.LOGIN, Token.decodeToken(rToken).getIssuer());
@@ -50,24 +43,8 @@ public class UserPairStop {
                     break label;
                 }
                 ObjectId userID = (ObjectId) user.getField(User.Field.ID);
-                Module module = (Module) Database.find_entity(Database.Collections.Modules, Module.Field.SESSION_ID, rSessionId);
-                if (module == null) {
-                    sending = new ResponseObject(true);
-                    sending.put(Protocol.Field.STATUS.key, Protocol.Status.SPORT_SESSION_BAD_SESSIONID.code);
-                    LogManager.write("Bad session id");
-                    break label;
-                }
-                ObjectId moduleID = (ObjectId) module.getField(Module.Field.ID);
-                SportSession moduleSession = (SportSession) Database.find_entity(Database.Collections.SportSessions, SportSession.Field.MODULE_ID, moduleID);
-                if (moduleSession != null) EndSportSession.end(moduleSession);
                 SportSession userSession = (SportSession) Database.find_entity(Database.Collections.SportSessions, SportSession.Field.USER_ID, userID);
                 if (userSession != null) EndSportSession.end(userSession);
-                userSession = (SportSession) Database.new_entity(Database.Collections.SportSessions);
-                userSession.setField(SportSession.Field.USER_ID, userID);
-                userSession.setField(SportSession.Field.MODULE_ID, moduleID);
-                userSession.setField(SportSession.Field.PRODUCTION, new ArrayList());
-                userSession.setField(SportSession.Field.EXPIRATION, LocalDateTime.now());
-                Database.update_entity(Database.Collections.SportSessions, userSession);
                 sending = new ResponseObject(false);
                 sending.put(Protocol.Field.STATUS.key, Protocol.Status.GENERIC_OK);
             } catch (Exception e) {
