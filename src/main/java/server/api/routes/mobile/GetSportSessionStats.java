35c322ccc013648e1eb125e9d66f8cf111e9859f
Hadrien DAURES <hadrien.daures@gmail..com>
2018-06-15 08:42:49 +0200

    je suis trop c_n

--- src/main/java/server/api/routes/mobile/GetSportSessionStats.java
+++ src/main/java/server/api/routes/mobile/GetSportSessionStats.java
@@ -64,7 +64,7 @@ public class GetSportSessionStats {
                     break label;
                 }
                 ObjectId sessionId = new ObjectId(rSessionId);
-                SportSession session = (SportSession) Database.find_entity(Database.Collections.SportSessions, SportSession.Field.ID, rSessionId);
+                SportSession session = (SportSession) Database.find_entity(Database.Collections.SportSessions_HISTORY, SportSession.Field.ID, rSessionId);
                 if (session == null) {
                     sending = new ResponseObject(true);
                     sending.put(Protocol.Field.STATUS.key, Protocol.Status.SPORT_SESSION_NO_SESSION.code);
