d0998b682a0934538917441fa956a035a9a39b02
Hadrien DAURES <hadrien.daures@gmail..com>
2018-08-04 14:26:20 +0200

    post create DONE

--- src/main/java/server/api/routes/mobile/post/PostCreate.java
+++ src/main/java/server/api/routes/mobile/post/PostCreate.java
@@ -2,9 +2,6 @@ package server.api.routes.mobile.post;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.Map;
-import java.util.Optional;
-
-import org.bson.types.ObjectId;
 
 import com.auth0.jwt.JWT;
 import com.google.gson.GsonBuilder;
@@ -16,9 +13,6 @@ import io.vertx.core.http.HttpServerResponse;
 import io.vertx.ext.web.Router;
 import model.Database;
 import model.Database.Collections;
-import model.entities.Fitness_Center;
-import model.entities.Fitness_Center_Manager;
-import model.entities.Picture;
 import model.entities.Post;
 import model.entities.User;
 import protocol.ResponseObject;
@@ -31,38 +25,67 @@ public class PostCreate {
             ResponseObject sending = null;
             HttpServerResponse response = routingContext.response().putHeader("content-type", "text/plain");
 
-            label:try {
+            try {
                 Map<String, Object> received = routingContext.getBodyAsJson().getMap();
                 String rToken = (String) received.get(Protocol.Field.TOKEN.key);
-
+                String rPostType = (String) received.get(Protocol.Field.POSTTYPE.key);
+                String rPostIcon = (String) received.get(Protocol.Field.POSTICON.key);
+                String rPostContent = (String) received.get(Protocol.Field.POSTCONTENT.key);
+                
                 if (rToken == null) {
                     sending = new ResponseObject(true);
                     sending.put(Protocol.Field.STATUS.key, Protocol.Status.GENERIC_KO.code);
                     LogManager.write("Missing key " + Protocol.Field.TOKEN.key);
-                    break label;
+                    return;
+                }
+                if (rPostType == null) {
+                    sending = new ResponseObject(true);
+                    sending.put(Protocol.Field.STATUS.key, Protocol.Status.GENERIC_KO.code);
+                    LogManager.write("Missing key " + Protocol.Field.POSTTYPE.key);
+                    return;
+                }
+                if (rPostIcon == null) {
+                    sending = new ResponseObject(true);
+                    sending.put(Protocol.Field.STATUS.key, Protocol.Status.GENERIC_KO.code);
+                    LogManager.write("Missing key " + Protocol.Field.POSTICON.key);
+                    return;
+                }
+                if (rPostContent == null) {
+                    sending = new ResponseObject(true);
+                    sending.put(Protocol.Field.STATUS.key, Protocol.Status.GENERIC_KO.code);
+                    LogManager.write("Missing key " + Protocol.Field.POSTCONTENT.key);
+                    return;
                 }
                 JWT token = Token.decodeToken(rToken);
                 if (token == null) {
                     sending = new ResponseObject(true);
                     sending.put(Protocol.Field.STATUS.key, Protocol.Status.AUTH_ERROR_TOKEN.code);
                     LogManager.write(Protocol.Status.AUTH_ERROR_TOKEN.message);
-                    break label;
+                    return;
                 }
                 User user = (User) Database.find_entity(Database.Collections.Users, User.Field.LOGIN, token.getIssuer());
                 if (user == null || !rToken.equals(user.getField(User.Field.TOKEN))) {
                     sending = new ResponseObject(true);
                     sending.put(Protocol.Field.STATUS.key, Protocol.Status.AUTH_ERROR_TOKEN.code);
                     LogManager.write(Protocol.Status.AUTH_ERROR_TOKEN.message);
-                    break label;
+                    return;
                 }
+                Post post = (Post) Database.new_entity(Collections.Posts);
+                post.setField(Post.Field.POSTERID, user.getField(User.Field.ID));
+                post.setField(Post.Field.TYPE, rPostType);
+                post.setField(Post.Field.PICTURE, rPostIcon);
+                post.setField(Post.Field.CONTENT, rPostContent);
+                Database.update_entity(Collections.Posts, post);
                 sending = new ResponseObject(false);
                 sending.put(Protocol.Field.STATUS.key, Protocol.Status.GENERIC_OK.code);
-            } catch (Exception e) {
+                sending.put(Protocol.Field.POSTID.key, post.getField(Post.Field.ID));
+            } catch (InvocationTargetException | NoSuchMethodException | InstantiationException | IllegalAccessException e) {
                 sending = new ResponseObject(true);
                 sending.put(Protocol.Field.STATUS.key, Protocol.Status.INTERNAL_SERVER_ERROR.code);
                 LogManager.write(e);
+            } finally {
+                response.end(new GsonBuilder().create().toJson(sending));
             }
-            response.end(new GsonBuilder().create().toJson(sending));
         });
     }
 }
