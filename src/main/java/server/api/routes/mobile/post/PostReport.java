7492e5db43dacf0f64a0a078cd49b56cdeb90bd3
hadrien <hadrien.daures@gmail.com>
2018-11-22 08:37:55 +0100

    reporting des posts

---
+++ src/main/java/server/api/routes/mobile/post/PostReport.java
@@ -0,0 +1,85 @@
+package server.api.routes.mobile.post;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.bson.types.ObjectId;
+
+import com.auth0.jwt.JWT;
+import com.google.gson.GsonBuilder;
+
+import Tools.LogManager;
+import Tools.Token;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Router;
+import model.Database;
+import model.Database.Collections;
+import model.entities.Post;
+import model.entities.User;
+import protocol.ResponseObject;
+import protocol.mobile.Protocol;
+
+public class PostReport {
+	public PostReport(Router router) {
+		router.route(HttpMethod.POST, Protocol.Path.POST_REPORT.path).handler(routingContext -> {
+
+			ResponseObject sending = null;
+			HttpServerResponse response = routingContext.response().putHeader("content-type", "text/plain");
+
+			try {
+				Map<String, Object> received = routingContext.getBodyAsJson().getMap();
+				String rToken = (String) received.get(Protocol.Field.TOKEN.key);
+				String rPostId = (String) received.get(Protocol.Field.POSTID.key);
+
+				if (rToken == null) {
+					sending = new ResponseObject(true);
+					sending.put(Protocol.Field.STATUS.key, Protocol.Status.GENERIC_KO.code);
+					LogManager.write("Missing key " + Protocol.Field.TOKEN.key);
+					return;
+				}
+				if (rPostId == null) {
+					sending = new ResponseObject(true);
+					sending.put(Protocol.Field.STATUS.key, Protocol.Status.GENERIC_KO.code);
+					LogManager.write("Missing key " + Protocol.Field.POSTID.key);
+					return;
+				}
+				JWT token = Token.decodeToken(rToken);
+				if (token == null) {
+					sending = new ResponseObject(true);
+					sending.put(Protocol.Field.STATUS.key, Protocol.Status.AUTH_ERROR_TOKEN.code);
+					LogManager.write(Protocol.Status.AUTH_ERROR_TOKEN.message);
+					return;
+				}
+				User user = (User) Database.find_entity(Database.Collections.Users, User.Field.LOGIN,
+						token.getIssuer());
+				if (user == null || !rToken.equals(user.getField(User.Field.TOKEN))) {
+					sending = new ResponseObject(true);
+					sending.put(Protocol.Field.STATUS.key, Protocol.Status.AUTH_ERROR_TOKEN.code);
+					LogManager.write(Protocol.Status.AUTH_ERROR_TOKEN.message);
+					return;
+				}
+				ObjectId postId = new ObjectId(rPostId);
+				Post post = (Post) Database.find_entity(Database.Collections.Posts, Post.Field.ID, postId);
+				if (post == null) {
+					sending = new ResponseObject(true);
+					sending.put(Protocol.Field.STATUS.key, Protocol.Status.POST_NOT_FOUND.code);
+					LogManager.write(Protocol.Status.POST_NOT_FOUND.message);
+					return;
+				}
+				post.setField(Post.Field.IS_REPORTED, true);
+				Database.update_entity(Collections.Posts, post);
+				sending = new ResponseObject(false);
+				sending.put(Protocol.Field.STATUS.key, Protocol.Status.GENERIC_OK.code);
+			} catch (Exception e) {
+				sending = new ResponseObject(true);
+				sending.put(Protocol.Field.STATUS.key, Protocol.Status.INTERNAL_SERVER_ERROR.code);
+				LogManager.write(e);
+			} finally {
+				response.end(new GsonBuilder().create().toJson(sending));
+			}
+		});
+	}
+}
