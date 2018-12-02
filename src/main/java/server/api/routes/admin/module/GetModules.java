dd4e93c562c5511dd1835244a52d1a96b7878269
Julien Longayrou <julien.longayrou@epitech.eu>
2018-12-02 16:56:38 +0100

    Update GetModules.java

--- src/main/java/server/api/routes/admin/module/GetModules.java
+++ src/main/java/server/api/routes/admin/module/GetModules.java
@@ -43,16 +43,12 @@ public class GetModules {
                     sending = new ResponseObject(false);
                     sending.put(Protocol.Field.STATUS.key, protocol.intranet.Protocol.Status.GENERIC_OK.code);
 
-                    LogManager.write("1");
-
                     @SuppressWarnings("unchecked")
                     FindIterable<Administrator> findIterableAdmin = (FindIterable<Administrator>) Database.collections.get(Database.Collections.Administrators).find();
                     Map<String,Object> admins = new HashMap<>();
                     for (Document doc : findIterableAdmin) {
                         admins.put(doc.getObjectId("_id").toString(), doc.getString("first_name") + " " + doc.getString("last_name"));
                     }
-
-                    LogManager.write("2");
 
                     @SuppressWarnings("unchecked")
                     FindIterable<Module> findIterable = (FindIterable<Module>) Database.collections.get(Database.Collections.Modules).find().sort(orderBy(descending(Module.Field.MODULE_STATE_CODE.get_key())));
@@ -62,40 +58,29 @@ public class GetModules {
                         cur = new HashMap<>();
                         cur.put("_id", doc.getObjectId("_id").toString());
                         cur.put("UUID", doc.getString("UUID"));
-                        LogManager.write("3");
                         if (doc.getObjectId("fitness_center_id") != null) {
                             cur.put("fitness_center_id", doc.getObjectId("fitness_center_id").toString());
                         } else {
                             cur.put("fitness_center_id", "");
                         }
-                        LogManager.write("4");
                         cur.put("machine_type", doc.getString("machine_type"));
-                        LogManager.write("5");
                         if (doc.getObjectId("module_state_id") != null) {
                             cur.put("module_state_id", doc.getObjectId("module_state_id").toString());
                         } else {
                             cur.put("module_state_id", "");
                         }
-                        LogManager.write("6");
                         cur.put("module_state_code", doc.getInteger("module_state_code"));
-                        LogManager.write("7");
                         cur.put("creation_date", doc.getLong("creation_date"));
-                        LogManager.write("8");
                         cur.put("update_date", doc.getLong("update_date"));
-                        LogManager.write("9");
                         if (doc.getObjectId("creator_admin_id") != null) {
                             cur.put("creator_admin_id", doc.getObjectId("creator_admin_id").toString());
                         } else {
                             cur.put("creator_admin_id", "");
                         }
-                        LogManager.write("10");
                         if (doc.getObjectId("creator_admin_id") != null &&
                                 admins.containsKey(doc.getObjectId("creator_admin_id").toString())) {
-                            LogManager.write("11");
                             cur.put("creator_admin_name", admins.get(doc.getObjectId("creator_admin_id").toString()));
                         }
-
-                        LogManager.write("12");
                         modules.add(cur);
                     }
                     sending.put(Protocol.Field.MODULES.key, modules);
