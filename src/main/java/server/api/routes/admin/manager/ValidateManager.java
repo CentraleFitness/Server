3a0b52cefc3bb35cceb928eeecabed9215714d52
Julien Longayrou <julien.longayrou@epitech.eu>
2018-12-10 20:27:20 +0100

    Mailing
    Mailing

--- src/main/java/server/api/routes/admin/manager/ValidateManager.java
+++ src/main/java/server/api/routes/admin/manager/ValidateManager.java
@@ -2,6 +2,7 @@ package server.api.routes.admin.manager;
 
 import Tools.LogManager;
 import Tools.ObjectIdSerializer;
+import Tools.OutlookInterface;
 import Tools.Token;
 import com.google.gson.GsonBuilder;
 import io.vertx.core.http.HttpMethod;
@@ -56,6 +57,13 @@ public class ValidateManager {
 
                         Long time = System.currentTimeMillis();
 
+                        String managerName = manager.getField(Fitness_Center_Manager.Field.FIRSTNAME) + " " +
+                                manager.getField(Fitness_Center_Manager.Field.LASTNAME);
+                        String adminName = admin.getField(Fitness_Center_Manager.Field.FIRSTNAME) + " " +
+                                admin.getField(Fitness_Center_Manager.Field.LASTNAME);
+                        String mailObject = "";
+                        String mailContent = "";
+
                         if ((Boolean)received.get(Protocol.Field.IS_VALIDATED.key)) {
                             manager.setField(Fitness_Center_Manager.Field.IS_ACTIVE, true);
                             manager.setField(Fitness_Center_Manager.Field.IS_VALIDATED, true);
@@ -63,10 +71,26 @@ public class ValidateManager {
 
                             manager.setField(Fitness_Center_Manager.Field.LAST_UPDATE_ACTIVITY, time);
                             manager.setField(Fitness_Center_Manager.Field.LAST_UPDATE_ADMIN_ID, admin.getField(Administrator.Field.ID));
+
+                            mailObject = "Validation de votre compte";
+                            mailContent = "Bonjour " + managerName + ",<br/><br/>" +
+                                    "Votre compte a été validé par " + adminName + ", administrateur Centrale Fitness !<br/><br/>" +
+                                    "Vous pouvez désormais accéder à votre espace Centrale Fitness et administrer votre salle.<br/><br/>" +
+                                    "A bientôt,<br/><br/>" +
+                                    "L'équipe Centrale Fitness";
+
                         } else {
                             manager.setField(Fitness_Center_Manager.Field.IS_ACTIVE, false);
                             manager.setField(Fitness_Center_Manager.Field.IS_VALIDATED, false);
                             manager.setField(Fitness_Center_Manager.Field.IS_REFUSED, true);
+
+                            mailObject = "Refus de votre compte";
+                            mailContent = "Bonjour " + managerName + ",<br/><br/>" +
+                                    "Votre compte a été refusé par " + adminName + ", administrateur Centrale Fitness.<br/><br/>" +
+                                    "Votre compte demeurera inactif à moins qu'un administrateur décide de revenir sur cette décision.<br/><br/>" +
+                                    "A bientôt peut être,<br/><br/>" +
+                                    "L'équipe Centrale Fitness";
+
                         }
 
                         manager.setField(Fitness_Center_Manager.Field.VALIDATION_DATE, time);
@@ -75,6 +99,12 @@ public class ValidateManager {
                         manager.setField(Fitness_Center_Manager.Field.VALIDATOR_ADMIN_IS_MANAGER, false);
 
                         Database.update_entity(Database.Collections.Fitness_Center_Managers, manager);
+
+                        OutlookInterface.outlookInterface.sendMail(
+                                (String)manager.getField(Fitness_Center_Manager.Field.EMAIL),
+                                mailObject,
+                                mailContent
+                        );
 
                         sending.put(Protocol.Field.ADMINISTRATOR_ID.key, admin.getField(Administrator.Field.ID));
                         sending.put(Protocol.Field.ADMINISTRATOR_NAME.key, admin.getField(Administrator.Field.FIRSTNAME) + " " + admin.getField(Administrator.Field.LASTNAME));
